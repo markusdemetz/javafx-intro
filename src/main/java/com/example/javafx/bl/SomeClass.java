f1a4551023ed666e4db05e4f02a6f7906998d94b
Markus Demetz <markus.demetz@synconia.com>
2022-03-20 17:19:31 +0100

    Cleanup.

--- src/main/java/com/example/javafx/bl/SomeClass.java
+++
@@ -1,6 +0,0 @@
-package com.example.javafx.bl;
-
-public class SomeClass {
-
-
-}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,7 @@
 module com.example.javafx {
     requires javafx.controls;
     requires javafx.fxml;
-    requires org.junit.jupiter.api;
 
     opens com.example.javafx to javafx.fxml;
-    opens com.example.javafx.bl to org.junit.platform.commons;
     exports com.example.javafx;
 }
